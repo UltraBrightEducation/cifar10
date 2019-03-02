30225179cd299f9b38b4df0227354420f375cc82
Kevin Huang <kevin.huang@changehealthcare.com>
2019-03-01 23:29:05 -0800

    train.py

--- model/model.py
+++ model/model.py
@@ -9,12 +9,11 @@ from keras.layers import (
     Flatten,
 )
 from keras.models import Model
-from keras.applications.resnet50 import ResNet50
 
 
 def _conv_block(x, filters, kernel_size, activation, pool=(2, 2)):
-    hidden = Conv2D(filters, kernel_size, activation=activation)(x)
-    hidden = Conv2D(filters, kernel_size, activation=activation)(hidden)
+    hidden = Conv2D(filters, kernel_size, activation=activation, padding="same")(x)
+    hidden = Conv2D(filters, kernel_size, activation=activation, padding="same")(hidden)
     hidden = MaxPool2D(pool_size=pool)(hidden)
     return hidden
 
@@ -33,23 +32,17 @@ def convnet7(
     conv_1 = _conv_block(
         image, filters=base_filters, kernel_size=3, activation=activation, pool=2
     )
-    conv_2 = _conv_block(
-        conv_1, filters=base_filters, kernel_size=3, activation=activation, pool=2
-    )
-    conv_2 = Dropout(dropout)(conv_2)
+    conv_2 = Dropout(dropout)(conv_1)
     conv_3 = _conv_block(conv_2, base_filters * 2, kernel_size=3, activation=activation)
     conv_4 = _conv_block(
         conv_3, base_filters * 2, kernel_size=3, activation=activation, pool=2
     )
-    conv_4 = Dropout(dropout)(conv_4)
-    conv_5 = _conv_block(conv_4, base_filters * 4, kernel_size=3, activation=activation)
-    conv_6 = _conv_block(
-        conv_5, base_filters * 4, kernel_size=3, activation=activation, pool=2
-    )
-    conv_6 = Dropout(dropout)(conv_6)
-    conv_7 = _conv_block(conv_6, base_filters * 2, kernel_size=1, activation=activation)
+    conv_5 = Dropout(dropout)(conv_4)
+    conv_6 = Conv2D(
+        base_filters * 2, kernel_size=1, activation=activation, padding="same"
+    )(conv_5)
 
-    hidden = GlobalMaxPooling2D()(conv_7)
+    hidden = GlobalMaxPooling2D()(conv_6)
     hidden = Dropout(dropout)(hidden)
 
     fc_1 = Dense(fc_size, activation=activation)(hidden)
--- model/train.py
+++ model/train.py
@@ -1,24 +1,27 @@
 import json
 import os
+import pathlib
 import shutil
 from argparse import ArgumentParser
 from datetime import datetime
 
+import keras
 import numpy as np
 from keras.callbacks import ModelCheckpoint, TensorBoard
 from keras_preprocessing.image import ImageDataGenerator
-from sklearn.model_selection import train_test_split
 from sklearn.utils import compute_class_weight
+from keras.datasets import cifar10
 
-from model.data import load_data
-from model.model import MODELS
+from model.model import convnet7
+
+MODELS = {"convnet7": convnet7}
 
 
 def get_class_weight(y_true):
     classes = np.arange(y_true.shape[-1])
-    class_counts = y_true.sum(0)
+    class_counts = y_true.sum(0).astype(np.int64)
     pos = 0
-    y_weight = np.zeros(class_counts.sum())
+    y_weight = np.zeros(int(class_counts.sum()))
     for i, count in enumerate(class_counts):
         y_weight[pos : pos + count] = i
         pos += count
@@ -28,8 +31,6 @@ def get_class_weight(y_true):
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--train-path")
-    parser.add_argument("--image-path")
     parser.add_argument("--model-name", choices=list(MODELS.keys()))
     parser.add_argument("--params-path")
     parser.add_argument("--artifact-directory")
@@ -41,23 +42,19 @@ if __name__ == "__main__":
         artifact_directory, "weights.{epoch:02d}-{val_loss:.6f}.hdf5"
     )
     log_dir = os.path.join(artifact_directory, "logs")
-    os.makedirs(log_dir)
+    pathlib.Path(log_dir).mkdir(parents=True, exist_ok=True)
     shutil.copy(args.params_path, artifact_directory)
 
-    X, y = load_data(args.train_path, args.image_path)
-    X_train, X_val, y_train, y_val = train_test_split(
-        X, y, test_size=0.2, random_state=42
-    )
-    del X, y
+    (X_train, y_train), (X_test, y_test) = cifar10.load_data()
+    print("x_train shape:", X_train.shape)
+    print(X_train.shape[0], "train samples")
+    print(X_test.shape[0], "test samples")
+
+    y_train = keras.utils.to_categorical(y_train, num_classes=10)
+    y_test = keras.utils.to_categorical(y_test, num_classes=10)
 
     mean, std = X_train.mean(), X_train.std()
     print("training mean: {} std: {}".format(mean, std))
-
-    X_train -= mean
-    X_train /= std
-
-    X_val -= mean
-    X_val /= std
 
     with open(args.params_path) as params_file:
         hyperparameters = json.load(params_file)
@@ -74,13 +71,47 @@ if __name__ == "__main__":
     model.summary()
     model.compile("adam", loss=hyperparameters["loss"], metrics=["acc"])
 
-    image_data_generator = ImageDataGenerator()
+    image_data_generator = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        zca_epsilon=1e-06,  # epsilon for ZCA whitening
+        rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+        # randomly shift images horizontally (fraction of total width)
+        width_shift_range=0.1,
+        # randomly shift images vertically (fraction of total height)
+        height_shift_range=0.1,
+        shear_range=0.0,  # set range for random shear
+        zoom_range=0.0,  # set range for random zoom
+        channel_shift_range=0.0,  # set range for random channel shifts
+        # set mode for filling points outside the input boundaries
+        fill_mode="nearest",
+        cval=0.0,  # value used for fill_mode = "constant"
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False,  # randomly flip images
+        # set rescaling factor (applied before any other transformation)
+        rescale=None,
+        # set function that will be applied on each input
+        preprocessing_function=None,
+        # image data format, either "channels_first" or "channels_last"
+        data_format=None,
+        # fraction of images reserved for validation (strictly between 0 and 1)
+        validation_split=0.0,
+    )
 
     batch_size = hyperparameters["batch_size"]
     model.fit_generator(
         generator=image_data_generator.flow(X_train, y_train, batch_size=batch_size),
         steps_per_epoch=len(X_train) // batch_size,
         epochs=100,
-        validation_data=(X_val, y_val),
+        validation_data=(X_test, y_test),
+        class_weight=get_class_weight(y_train),
         callbacks=[ModelCheckpoint(checkpoint_format), TensorBoard(log_dir=log_dir)],
     )
+
+    # Score trained model.
+    scores = model.evaluate(X_test, y_test, verbose=1)
+    print("Test loss:", scores[0])
+    print("Test accuracy:", scores[1])
